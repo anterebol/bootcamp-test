39242625d2927135a90bad006f7ed9aefc5255fc
Aleks <anterebol@gmail.com>
2024-01-16 22:58:49 +0300

    fead: update character card

--- src/components/CharactersList/CharacterCard/CharacterCard.tsx
+++ src/components/CharactersList/CharacterCard/CharacterCard.tsx
@@ -1,3 +1,19 @@
-export const CharacterCard = () => {
-  return <li className="character-card"></li>
-}
+import './characterCard.scss';
+import { CharacterType } from '../../../types/characterType';
+
+export const CharacterCard = (props: {
+  key: string;
+  character: CharacterType;
+}) => {
+  const { character } = props;
+  const { name, image } = character;
+
+  return (
+    <li className="character-card">
+      <div className="character-card__img">
+        <img src={image} alt={`${name}-img`} />
+      </div>
+      <p className="character-card__name">{name}</p>
+    </li>
+  );
+};
