a18b2c54a7aa82e140cc90fa04a2e5a13d931ff6
Aleks <anterebol@gmail.com>
2024-01-16 20:57:03 +0300

    refactor: update Button with joinClasses utils

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,11 @@
-import { ButtonType } from "../../types/buttonType";
+import { ButtonType } from '../../types/buttonType';
+import { joinClasses } from '../../utils/joinClasses';
 
 export const Button = (props: ButtonType) => {
   const { cls, children, callback } = props;
-  return <button className={cls?.join(' ')} onClick={callback}>
-    {children}
-  </button>
-}
+  return (
+    <button type="button" className={joinClasses(cls || [])} onClick={callback}>
+      {children}
+    </button>
+  );
+};
