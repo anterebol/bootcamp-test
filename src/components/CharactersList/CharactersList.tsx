b200ba8aa19296a17dbfbbe1cfa6e8182f04b041
Aleks <anterebol@gmail.com>
2024-01-16 22:59:34 +0300

    feat: update list with characters

--- src/components/CharactersList/CharactersList.tsx
+++ src/components/CharactersList/CharactersList.tsx
@@ -1,5 +1,14 @@
-import './listCards.scss';
+import { useAppSelector } from '../../hooks/hooks';
+import { CharacterCard } from './CharacterCard/CharacterCard';
+import './charactersList.scss';
 
 export const CharactersList = () => {
-  return <ul></ul>
+  const { characters } = useAppSelector((state) => state.appReducer);
+  return (
+    <ul className="list-characters">
+      {characters.map((character) => {
+        return <CharacterCard key={character.id} character={character} />;
+      })}
+    </ul>
+  );
 };
