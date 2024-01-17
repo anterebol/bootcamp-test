5fdaa9a8971b9306e32c2c056e05523904cd7234
Aleks <anterebol@gmail.com>
2024-01-17 22:44:14 +0300

    feat: add feature for opening popup by click on the character card

--- src/components/CharactersList/CharacterCard/CharacterCard.tsx
+++ src/components/CharactersList/CharacterCard/CharacterCard.tsx
@@ -1,15 +1,23 @@
 import './characterCard.scss';
 import { CharacterType } from '../../../types/characterType';
+import { useAppDispatch } from '../../../hooks/hooks';
+import { openCharacterPopup } from '../../../store/appReducer/appReducer';
 
 export const CharacterCard = (props: {
   key: string;
   character: CharacterType;
 }) => {
   const { character } = props;
-  const { name, image } = character;
+  const { name, image, id } = character;
+  const dispath = useAppDispatch();
 
   return (
-    <li className="character-card">
+    <li
+      className="character-card"
+      onClick={() => {
+        dispath(openCharacterPopup(id));
+      }}
+    >
       <div className="character-card__img">
         <img src={image} alt={`${name}-img`} />
       </div>
