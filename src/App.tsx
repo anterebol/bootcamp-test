528067b93341b8162431141d9948fab0025dabcb
Aleks <anterebol@gmail.com>
2024-01-17 22:49:55 +0300

    feat: update app with Popup and CharacterList components and request to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
 import './App.scss';
+import { useAppDispatch, useAppSelector } from './hooks/hooks';
+import { CharactersList } from './components/CharactersList/CharactersList';
+import { getCharacters } from './store/api/api';
+import { Popup } from './components/Popup/Popup';
 
 function App() {
+  const dispatch = useAppDispatch();
+  const { currentPage } = useAppSelector((state) => state.appReducer);
+  useEffect(() => {
+    dispatch(getCharacters(currentPage));
+  }, [currentPage, dispatch]);
   return (
-    <div>
-      <h1>IT Bootcamp test</h1>
-    </div>
+    <>
+      <Popup />
+      <CharactersList />
+    </>
   );
 }
 
