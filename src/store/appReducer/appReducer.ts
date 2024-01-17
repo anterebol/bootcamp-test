9dda37b96dc5c18b58124ef4f154b1c056788189
Aleks <anterebol@gmail.com>
2024-01-17 22:41:44 +0300

    feat: add functions for opening and closing popup into reducer and refactor some cases into extraReducers

--- src/store/appReducer/appReducer.ts
+++ src/store/appReducer/appReducer.ts
@@ -1,46 +1,58 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { CharacterType } from "../../types/characterType";
-import { getCharacters } from "../api/api";
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+import { CharacterType } from '../../types/characterType';
+import { getCharacters } from '../api/api';
 
 const initialState = {
   currentPage: 1,
   characters: [] as Array<CharacterType>,
   loading: false,
+  currentCharacterId: null as string | null,
+  openedPopup: false,
   pages: 1,
   isPagination: false,
-}
+};
 
 export const appSlice = createSlice({
   name: 'reducer',
-  initialState: {...initialState},
+  initialState: { ...initialState },
   reducers: {
     setPage: (state, action) => {
       state.currentPage = action.payload;
     },
     togglePagination: (state) => {
       state.isPagination = !state.isPagination;
-    }
+    },
+    openCharacterPopup: (state, action) => {
+      state.currentCharacterId = action.payload;
+      state.openedPopup = true;
+    },
+    closePopup: (state) => {
+      state.openedPopup = false;
+    },
   },
   extraReducers: (builder) => {
-    builder.addCase(getCharacters.pending, (state) => {
-      state.loading = true;
-    })
-    .addCase(
-      getCharacters.fulfilled,
-      (state, action) => {
-        const {pages, data} = action.payload || [];
+    builder
+      .addCase(getCharacters.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getCharacters.fulfilled, (state, action) => {
+        const { pages, characters } = action.payload || [];
         if (state.isPagination) {
-          state.characters = [...data];
+          state.characters = [...characters];
         } else {
-          state.characters = [...state.characters, ...data];
+          state.characters = [
+            ...state.characters,
+            ...characters,
+          ] as CharacterType[];
         }
         state.pages = pages;
-      }
-    )
-    .addCase(getCharacters.rejected, (state) => {
-      state.loading = true;
-    })
+      })
+      .addCase(getCharacters.rejected, (state) => {
+        state.loading = true;
+      });
   },
 });
 export default appSlice.reducer;
-export const {setPage, togglePagination} = appSlice.actions;
+export const { setPage, togglePagination, openCharacterPopup, closePopup } =
+  appSlice.actions;
