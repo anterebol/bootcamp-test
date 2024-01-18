33819ba5a1f7507bff2c5f05fca7f49aede9c6f6
Aleks <anterebol@gmail.com>
2024-01-18 18:22:03 +0300

    feat: add scroliing uploading

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,43 @@ import { getCharacters } from './store/api/api';
 import { Popup } from './components/Popup/Popup';
 import { Pagination } from './components/Pagination/Pagination';
 import { Header } from './components/Header/Header';
+import { setPage } from './store/appReducer/appReducer';
+import { Button } from './components/Button/Button';
+import top from './assets/arrows/top.svg';
 
 function App() {
   const dispatch = useAppDispatch();
   const { currentPage, isPagination } = useAppSelector(
     (state) => state.appReducer
   );
+
+  const setPageByScrolling = () => {
+    const scrollHeight = Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.body.clientHeight,
+      document.documentElement.clientHeight
+    );
+    const screenHeight = window.innerHeight;
+    const scrolled = window.scrollY;
+    const position = scrolled + screenHeight;
+    if (position + 50 >= scrollHeight) {
+      dispatch(setPage(currentPage + 1));
+    }
+  };
+
   useEffect(() => {
+    if (!isPagination) {
+      window.addEventListener('scroll', setPageByScrolling);
+    }
     dispatch(getCharacters(currentPage));
-  }, [currentPage, dispatch]);
+    return () => {
+      window.removeEventListener('scroll', setPageByScrolling);
+    };
+  }, [currentPage, isPagination]);
+
   return (
     <>
       <Header />
@@ -23,6 +51,17 @@ function App() {
         <CharactersList />
         {isPagination ? <Pagination /> : null}
       </main>
+      <Button
+        cls={['top-button']}
+        callback={() => {
+          document.getElementsByTagName('html')[0]?.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          });
+        }}
+      >
+        <img src={top} alt="top" />
+      </Button>
     </>
   );
 }
