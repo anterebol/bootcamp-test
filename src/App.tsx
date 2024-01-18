795dd6b2db86133f26ee187e9f0d262952b96eed
Aleks <anterebol@gmail.com>
2024-01-18 13:39:15 +0300

    feat: add Header and Pagination into App

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,25 @@ import { useAppDispatch, useAppSelector } from './hooks/hooks';
 import { CharactersList } from './components/CharactersList/CharactersList';
 import { getCharacters } from './store/api/api';
 import { Popup } from './components/Popup/Popup';
+import { Pagination } from './components/Pagination/Pagination';
+import { Header } from './components/Header/Header';
 
 function App() {
   const dispatch = useAppDispatch();
-  const { currentPage } = useAppSelector((state) => state.appReducer);
+  const { currentPage, isPagination } = useAppSelector(
+    (state) => state.appReducer
+  );
   useEffect(() => {
     dispatch(getCharacters(currentPage));
   }, [currentPage, dispatch]);
   return (
     <>
-      <Popup />
-      <CharactersList />
+      <Header />
+      <main className="main">
+        <Popup />
+        <CharactersList />
+        {isPagination ? <Pagination /> : null}
+      </main>
     </>
   );
 }
